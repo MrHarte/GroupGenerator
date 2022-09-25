a6ade8a113c571efc5d40b7800a78062072bedea
Munir Khaliqyar <LE2176022@crc-lennox.qc.ca>
2022-09-25 19:03:32 -0400

    Created a class named Student and added fields, constructor and properties.

---
+++ GroupGenerator/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGenerator
+{
+    internal class Student : Person
+    {
+
+        private int _displayMode;
+
+        /// <summary>
+        /// Gets or sets the name of the person.
+        /// </summary>
+        public override string Name { get; set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public Student(string id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
