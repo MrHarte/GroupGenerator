5f33704f145a635354c37ca7d7feaca0b0be36ba
Emile Boulet <LE2072910@crc-lennox.qc.ca>
2022-09-26 10:39:45 -0400

    Changing some code to make my idea work
    Testing some code and possible solutions

--- GroupGenerator/GroupsForm.cs
+++ GroupGenerator/GroupsForm.cs
@@ -30,8 +30,8 @@ namespace GroupGenerator
             this.InitializeComponent();
             this.personList = new BindingList<Person>();
             this.groupsListBox.DataSource = this.personList;
-            this.groupsListBox.DisplayMember = "Name";
             this.rng = new Random();
+            this.layout1RadioButton.Checked = true;
             this.EditListButton_Click(this, new EventArgs());
         }
 
@@ -70,26 +70,17 @@ namespace GroupGenerator
         private void LoadExampleListButton_Click(object sender, EventArgs e)
         {
             this.personList.Clear();
-            this.personList.Add(new Person("Ted Mosby"));
-            this.personList.Add(new Person("Marshall Eriksen"));
-            this.personList.Add(new Person("Robin Scherbatsky"));
-            this.personList.Add(new Person("Barney Stinson"));
-            this.personList.Add(new Person("Lily Aldrin"));
-            this.personList.Add(new Person("Tracy McConnell"));
-            this.personList.Add(new Person("Ranjit Singh"));
-            this.personList.Add(new Person("Sandy Rivers"));
-            this.personList.Add(new Person("Carl MacLaren"));
-            this.personList.Add(new Person("Marvin Eriksen"));
-            this.personList.Add(new Person("Judy Eriksen"));
-            this.personList.Add(new Person("Gary Blauman"));
-            this.personList.Add(new Person("Loretta Stinson"));
-            this.personList.Add(new Person("Stella Zinman"));
-            this.personList.Add(new Person("Randy Wharmpess"));
-            this.personList.Add(new Person("Tony Grafanello"));
-            this.personList.Add(new Person("Zoey Pierson"));
-            this.personList.Add(new Person("Jerome Whittaker"));
-            this.personList.Add(new Person("Quinn Garvey"));
-            this.personList.Add(new Person("William Zabka"));
+            this.personList.Add(new Student("Mosby, Ted Evelyn (6978639)"));
+            this.personList.Add(new Student("Eriksen, Marshall (6961326)"));
+            this.personList.Add(new Student("Scherbatsky,Robin (6375003)"));
+            this.personList.Add(new Student("Stinson,Barney (6236471)"));
+            this.personList.Add(new Student("Aldrin, Lily (0756495)"));
+            this.personList.Add(new Student("McConnell,Tracy (7936213)"));
+            this.personList.Add(new Student("Singh,Ranjit (3874496)"));
+            this.personList.Add(new Student("Rivers,Sandy (4193406)"));
+            this.personList.Add(new Student("MacLaren, Carl (5965287)"));
+            this.personList.Add(new Student("Eriksen,Marvin(1085283)"));
+            this.personList.Add(new Student("Judy Eriksen (123124423)"));
             this.personList.Shuffle();
         }
 
---
+++ GroupGenerator/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGenerator
+{
+    /// <summary>
+    /// Student class will adapt how the student names will show in the program.
+    /// </summary>
+    internal class Student : Person
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private static int DisplayNumber = 1;
+
+
+        public Student(string input)
+        {
+            input = input.Trim();
+        }
+
+        //Methods
+
+        //Sudent (name: string)
+        //Student (FirstName, LastName)
+
+        public override string ToString()
+        {
+            if (DisplayNumber == 1)
+            {
+
+            }
+            return this.Name;
+        }
+
+    }
+}
