25b8ef168c1ca950778c0fcc52946dd635a458c5
Michael Pye <LE1770426@crc-lennox.qc.ca>
2022-09-26 18:39:17 -0400

    Added Student.cs with variables and constructors, updating ImportForm to match variables present in Student.cs

---
+++ GroupGenerator/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace GroupGenerator
+{
+    internal class Student : Person
+    {
+        public Student()
+        {
+            Name = " ";
+            LastName = " ";
+            Id = " ";
+            TextMode = 0;
+        }
+
+        public Student(string lastName, string name, string id, int textMode)
+        {
+            LastName = lastName;
+            Name = name;
+            Id = id;
+            TextMode = TextMode;
+        }
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public static int TextMode { get; set; }
+    }
+
+    public Student(string nameCollecter = "")
+    {
+        
+    }
+
+    public override string ToString()
+    {
+
+    }
+
+
+
+
+
+
+} 
