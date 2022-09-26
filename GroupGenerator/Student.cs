edafd416fe582e55ebcfdfb2798823374be82077
Michael Pye <LE1770426@crc-lennox.qc.ca>
2022-09-26 19:48:36 -0400

    Completed XML Documentation

--- GroupGenerator/Student.cs
+++ GroupGenerator/Student.cs
@@ -7,8 +7,32 @@ using System.Xml.Linq;
 
 namespace GroupGenerator
 {
-    internal class Student : Person
+    /// <summary>
+    /// This is an internal class that is the child to the Person class
+    /// </summary>
+    /// <remarks></remarks>
+    public class Student : Person
     {
+        /// <summary>
+        /// Name of the student
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Last name of the student 
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Id of the student
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// Static int property that hold which textmode is selected
+        /// </summary>
+        public static int TextMode { get; set; }
+
+        /// <summary>
+        ///  Holds all null values for all varibles 
+        /// </summary>
         public Student()
         {
             Name = " ";
@@ -17,6 +41,13 @@ namespace GroupGenerator
             TextMode = 0;
         }
 
+        /// <summary>
+        /// Non parameterized constructors
+        /// </summary>
+        /// <param name="lastName">Non parameterized constructor for lastName</param>
+        /// <param name="name">Non parameterized constructor for name</param>
+        /// <param name="id">Non parameterized constructor for id</param>
+        /// <param name="textMode">Non parameterized constructor for textMode</param>
         public Student(string lastName, string name, string id, int textMode)
         {
             LastName = lastName;
@@ -24,18 +55,21 @@ namespace GroupGenerator
             Id = id;
             TextMode = TextMode;
         }
-
-        public string Name { get; set; }
-        public string LastName { get; set; }
-        public string Id { get; set; }
-        public static int TextMode { get; set; }
     }
-
+    /// <summary>
+    /// basis for tokenizing strings
+    /// </summary>
+    /// <param name="nameCollecter">varible that hold strings in importform</param>
+    /// <remarks> This section is incomplete </remarks>
     public Student(string nameCollecter = "")
     {
         
     }
-
+    /// <summary>
+    /// ToString method turns class into strings
+    /// </summary>
+    /// <returns>class properites as string</returns>
+    /// <remarks> This section is incomplete </remarks>
     public override string ToString()
     {
 
