900cb80f55dfc4336b3179be5c54e06b60b8ed67
Elijah Frechette <LE1837182@crc-lennox.qc.ca>
2022-09-28 15:34:56 -0400

    Starting the Student Class code and documentation

---
+++ GroupGenerator/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGenerator
+{
+    /// <summary>
+    /// Student class, derived from the Person class
+    /// </summary>
+    public class Student : Person
+    {
+        /// <summary>
+        /// Holds the first name field
+        /// </summary>
+        public string FirstName { get; set; }
+        
+        /// <summary>
+        /// Holds the last name field
+        /// </summary>
+        public string LastName { get; set; }
+        /// <summary>
+        /// Holds the student ID field
+        /// </summary>
+        public string StudentID { get; set; }
+        /// <summary>
+        /// Holds the int that decides the display mode
+        /// </summary>
+        public int DisplayMode { get; set; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="name"></param>
+        public Student(string name)
+        {
+            FirstName = " ";
+            LastName = " ";
+            DisplayMode = 0;
+            name = FirstName + LastName;
+            Random rand = new Random();
+            int number = rand.Next();
+            this.StudentID = rand.Next(0, 10000000).ToString("D7");
+
+        }
+    }
+}
